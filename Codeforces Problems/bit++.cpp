2939c7c89dfb83d0425251c7745216859bc3404b
Abhishek Sharma <sharmaabhi91011@gmail.com>
2020-06-05 16:25:28 +0530

    Add files via upload

---
+++ Codeforces Problems/bit++.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string p;
+    int a = 0,t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        cin >> p;
+        if (p[1] == '+')
+        {   
+            a++;
+        }
+        else
+        {
+            a--;
+        }
+    }
+    cout << a;
+    return 0;
+}
